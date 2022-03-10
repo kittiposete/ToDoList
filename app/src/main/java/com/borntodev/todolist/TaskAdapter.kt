775b185e868869238ee763e55fb611cb396cf2b9
kittiposete <kittipos.l@hotmail.com>
2022-03-10 17:19:34 +0700

    Update TaskAdapter.kt

--- app/src/main/java/com/borntodev/todolist/TaskAdapter.kt
+++ app/src/main/java/com/borntodev/todolist/TaskAdapter.kt
@@ -1,4 +1,38 @@
 package com.borntodev.todolist
 
-class TaskAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.borntodev.todolist.TaskAdapter.ViewHolder
+
+class TaskAdapter(data:ArrayList<TaskClass>, context: Context): RecyclerView.Adapter<ViewHolder>() {
+
+    private var data: ArrayList<TaskClass> ?= null
+    private var context:Context ?= null
+
+    init {
+        this.data = data
+        this.context = context
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view =LayoutInflater.from(context).inflate(R.layout.task_row, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return data!!.size
+    }
+
+    class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+        fun dataBind(data:TaskClass){
+
+        }
+    }
 }
