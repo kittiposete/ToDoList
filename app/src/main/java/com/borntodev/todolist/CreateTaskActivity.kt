a7797c6a5b72d29134ccf4e5ab173575a3880b7c
kittiposete <kittipos.l@hotmail.com>
2022-03-12 12:02:56 +0700

    create back button in createTaskActivity

--- app/src/main/java/com/borntodev/todolist/CreateTaskActivity.kt
+++ app/src/main/java/com/borntodev/todolist/CreateTaskActivity.kt
@@ -7,5 +7,7 @@ class CreateTaskActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_task)
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
 }
